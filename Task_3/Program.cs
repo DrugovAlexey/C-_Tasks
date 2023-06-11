4c0c72472052c2a0500ece03ad2797de8189ee4b
Olga <panda146@yandex.ru>
2023-06-11 20:22:09 +0300

    added task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Напишите целое число");
+string input_string1 = Console.ReadLine();
+int number = Convert.ToInt32(input_string1);
+
+if (number  == 0)
+{
+    Console.WriteLine($"Число {number} является четным числом, в википедии так написано))");
+}
+
+if (number != 0 & number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} четное");
+}
+if (number != 0 & number % 2 != 0)
+{
+    Console.WriteLine($"Число {number} нечетное");
+}
+
