dbcd852c502446437d5b997e69ad0f0c39c3086c
Olga <panda146@yandex.ru>
2023-06-11 20:25:11 +0300

    added task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Напишите целое положительное число: ");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = 2;
+
+if (number2 > number1)
+{
+    Console.WriteLine($"Нет четных положительных значений между 1 и числом  {number1} ");
+    
+}
+while (number2 <= number1)
+{
+    Console.Write($"{number2} ");
+    number2+=2;  
+}
