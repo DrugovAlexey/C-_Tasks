7b5e410f68bc72e97a7ec7150a2a0df9914c11a9
Olga <panda146@yandex.ru>
2023-06-11 20:15:42 +0300

    added task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Напишите первое целое число");
+string input_string1 = Console.ReadLine();
+Console.WriteLine("Напишите второе целое число");
+string input_string2 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = Convert.ToInt32(input_string2);
+if (number2 > number1)
+{
+    Console.WriteLine($"Число {number2} больше, а число {number1} меньше");
+}
+else
+{
+    Console.WriteLine($"Число {number1} больше, а число {number2} меньше");
+}
+if (number2 == number1)
+{
+    Console.WriteLine($"Первое число {number2} и второе число {number1} одинаковые");
+}
